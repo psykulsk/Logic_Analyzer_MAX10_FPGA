dfac2f4a434de3b5373d5b8eadd2f9e63ce2358d
Piotr Sykulski <pitersk@gmail>
2017-01-04 19:00:46 +0100

    Programmed and tested TriggerBlock.

--- TriggerBlock.v
+++ TriggerBlock.v
@@ -1,7 +1,7 @@
 module TriggerBlock (
 	dataIn,
 	triggerMask,
-	clk,
+	clk_PLL,
 	reset,
 	triggerOut,
 	dataOut
@@ -11,7 +11,7 @@ module TriggerBlock (
 	input [2:0] dataIn;
 	input [2:0] triggerMask;
 	input reset;
-	input clk;
+	input clk_PLL;
 	output triggerOut;
 	output [2:0] dataOut;
 	
@@ -19,11 +19,56 @@ module TriggerBlock (
 	wire [2:0] dataIn;
 	wire [2:0] triggerMask;
 	wire reset;
-	wire clk;
-	reg [2:0] dataOut;
+	wire clk_PLL;
+	wire [2:0] dataOut;
 	reg triggerOut;
 	
-
+		
+	reg [2:0] newDataReg;
+	reg [2:0] prevDataReg;
+	reg [2:0] differenceReg;
+	reg [2:0] triggerMaskReg;
 	
+	parameter triggerBlockOn = 0, triggerBlockOff = 1;
+	
+	reg triggerBlockState;
+	
+	assign dataOut = newDataReg;
+	
+	always @( posedge clk_PLL)
+	begin
+		if ( triggerBlockState == triggerBlockOn)
+			begin
+				prevDataReg = newDataReg;
+				newDataReg = dataIn;
+				differenceReg = prevDataReg^newDataReg;
+				triggerOut = |(differenceReg&triggerMask);
+			end
+		else if ( triggerBlockState == triggerBlockOff)
+			begin
+				newDataReg = dataIn;
+				triggerOut = 0;
+			end
+		else
+			begin
+				newDataReg = dataIn;
+				triggerOut = 0;
+			end
+		if(triggerOut == 1)
+			triggerBlockState <= triggerBlockOff;
+		
+	end
+	
+
+	always @( posedge reset)
+	begin
+			triggerBlockState <= triggerBlockOn;
+	end
+	
+	
+	always @(triggerMask)
+	begin
+		triggerMaskReg = triggerMask;
+	end
 	
 	endmodule
