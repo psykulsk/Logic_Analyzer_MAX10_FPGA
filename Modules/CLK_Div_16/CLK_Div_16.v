5eb65680cb84f1835aa3eed29f3bca4f5889a9e9
Piotr Sykulski <pitersk@gmail>
2017-01-10 20:11:57 +0100

    Connected system schematic. Create and tested CLK_Div_16.

---
+++ Modules/CLK_Div_16/CLK_Div_16.v
@@ -0,0 +1,21 @@
+module CLK_Div_16(
+	clk_in,
+	reset,
+	clk_div16
+	);
+	
+input wire clk_in;
+input wire reset;
+output reg clk_div16;
+
+reg [3:0] counter;
+
+always @(posedge clk_in or posedge reset)
+begin
+	if(reset == 1'b1)
+		counter = 0;
+	counter = counter+ 1;
+	clk_div16 = counter[3];
+end
+
+endmodule
